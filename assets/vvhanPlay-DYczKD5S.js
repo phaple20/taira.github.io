ff182e6251de3e569d11f83579056fb58e4b71ad
phaple20 <40193618+phaple20@users.noreply.github.com>
2024-05-19 00:31:43 +0800

    Delete assets directory

--- assets/vvhanPlay-DYczKD5S.js
+++
@@ -1,1 +0,0 @@
-import{_ as n}from"./index-C9-VaW_J.js";const r={};function a(c,e){return" vvhanpaly "}const _=n(r,[["render",a]]);export{_ as default};
--- assets/weather-iJmt7dIL.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-C9-VaW_J.js";const r={};function t(c,_){return" 天气 "}const o=e(r,[["render",t]]);export{o as default};
