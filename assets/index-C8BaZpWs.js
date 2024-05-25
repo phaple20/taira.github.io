f76c255dbdf4b39c6660eae7aa88f4a900782fde
phaple20 <40193618+phaple20@users.noreply.github.com>
2024-05-25 23:10:34 +0800

    Delete assets directory

--- assets/index-C8BaZpWs.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CCyMqpJO.js";function r(){return t.jsx("div",{children:"about"})}export{r as default};
--- assets/index-DqKsg6H2.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CCyMqpJO.js";function r(){return e.jsx("div",{children:"home"})}export{r as default};
