240031ffb83f270723c4544a594ef54b134b83b5
phaple20 <40193618+phaple20@users.noreply.github.com>
2024-05-25 11:50:07 +0800

    Delete assets directory

--- assets/index-BFJEgx2X.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Dt1BMpfj.js";function r(){return e.jsx("div",{children:"home"})}export{r as default};
--- assets/index-BuKdvU9z.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Dt1BMpfj.js";function r(){return t.jsx("div",{children:"about"})}export{r as default};
