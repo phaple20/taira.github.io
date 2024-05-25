d6fb42f38921052dc4cd80900dc5808d17fb7bd9
phaple20 <40193618+phaple20@users.noreply.github.com>
2024-05-25 12:00:09 +0800

    Delete assets directory

--- assets/index-AxJXxTAX.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Car_kgDd.js";function r(){return e.jsx("div",{children:"home"})}export{r as default};
--- assets/index-pwzZJbZs.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Car_kgDd.js";function r(){return t.jsx("div",{children:"about"})}export{r as default};
