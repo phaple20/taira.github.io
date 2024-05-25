f5aaa731f16602498e960a80b9076deef1f3bc3e
phaple20 <40193618+phaple20@users.noreply.github.com>
2024-05-25 11:44:56 +0800

    Add files via upload

---
+++ assets/index-BFJEgx2X.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Dt1BMpfj.js";function r(){return e.jsx("div",{children:"home"})}export{r as default};
---
+++ assets/index-BuKdvU9z.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Dt1BMpfj.js";function r(){return t.jsx("div",{children:"about"})}export{r as default};
